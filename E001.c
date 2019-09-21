e5cb2f5993d9e936b58fd89cba9924ce92557a00
lonlng <lonlng@163.com>
2019-09-21 15:53:01 +0800

    编写001,并编译，已完成

--- E001.c
+++ E001.c
@@ -1,15 +1,19 @@
+//题目：有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
+
+// 程序分析：可填在百位、十位、个位的数字都是1、2、3、4。组成所有的排列后再去 掉不满足条件的排列。
+
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-	for (int i = 0; i < 4; i++)
+	for (int i = 1; i < 5; i++)
 	{
-		for (int j = 0; j < i; j++)
+		for (int j = 1; j < 5; j++)
 		{
-			for (int k = 0; k < j; k++)
+			for (int k = 1; k < 5; k++)
 			{
-				for (int l = 0; l < k; l++)
+				if (i != j && j != k && k != i)
 				{
-					printf("%d%d%d%d\n", i, j, k, l);
+					printf("%d%d%d\n", i, j, k);
 				}
 			}
 		}
